dcc4d224c8e07568d7056fe7b0471a2186e15a14
Neriya Rosner <neri.coder@gmail.com>
2023-07-14 13:57:56 +0300

    rename function

--- src/features/users/repositories/UsersApiRepository.ts
+++ src/features/users/repositories/UsersApiRepository.ts
@@ -3,6 +3,6 @@ import {ApiUser} from "@/features/users/models/ApiUser";
 import {API_BASE_URL} from "@/features/users/repositories/consts";
 
 
-export const GetAllUsersFromApi = (): Promise<ApiUser[]> => {
+export const getUsersFromApi = (): Promise<ApiUser[]> => {
     return restGetRequest(API_BASE_URL);
 }
