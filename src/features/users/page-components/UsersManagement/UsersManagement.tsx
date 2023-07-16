9abcb1c2b6716bff91c99500251d80d3989126a7
Neriya Rosner <neriyarosner@gmail.com>
2023-07-16 14:38:08 +0300

    create user management page

---
+++ src/features/users/page-components/UsersManagement/UsersManagement.tsx
@@ -0,0 +1,7 @@
+export const UsersManagementPage = () => {
+    return (
+        <div>
+            <h1>Users Management</h1>
+        </div>
+    )
+}
---
+++ src/pages/users-management.tsx
@@ -0,0 +1,5 @@
+import {UsersManagementPage} from "@/features/users/page-components/UsersManagement";
+
+export default function UsersManagement() {
+    return <UsersManagementPage/>;
+}
