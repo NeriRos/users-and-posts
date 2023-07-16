f8394c81dde0b8c96ad546004e36510029918f3c
Neriya Rosner <neriyarosner@gmail.com>
2023-07-16 15:35:50 +0300

    design table

--- src/features/users/modules/UsersManagement/components/UsersTable/UsersTable.tsx
+++ src/features/users/modules/UsersManagement/components/UsersTable/UsersTable.tsx
@@ -1,11 +1,26 @@
-import {useQueryClient} from "@tanstack/react-query";
+import React from "react";
+import {useUsersTable} from "@/features/users/modules/UsersManagement/components/UsersTable/useUsersTable";
+import {User} from "@/features/users/models/User";
+import {UsersTableRow} from "@/features/users/modules/UsersManagement/components/UsersTable/UsersTableRow";
+
+import Styles from './UsersTable.module.css';
 
 export const UsersTable = () => {
     const queryClient = useQueryClient()
 
     return (
-        <div>
-            <h1>Users Table</h1>
-        </div>
+        <table className={Styles.table}>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            {users.map((user: User) => <UsersTableRow key={user.id} user={user}/>)}
+            </tbody>
+        </table>
     )
 }
