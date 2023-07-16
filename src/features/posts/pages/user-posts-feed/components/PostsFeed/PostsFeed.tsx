dbe9a3fe8729fba8c2b3596c1f1caff84ad73c44
Neriya Rosner <neriyarosner@gmail.com>
2023-07-16 20:41:06 +0300

    use posts search

--- src/features/posts/pages/user-posts-feed/components/PostsFeed/PostsFeed.tsx
+++ src/features/posts/pages/user-posts-feed/components/PostsFeed/PostsFeed.tsx
@@ -6,14 +6,23 @@ import {PostFeedItem} from "@/features/posts/pages/user-posts-feed/components/Po
 import {TEXTS} from "@/features/posts/pages/user-posts-feed/components/PostsFeed/texts";
 import {Post} from "@/features/posts/models/Post";
 import clsx from "clsx";
+import {PostsSearch} from "@/features/posts/pages/user-posts-feed/components/PostsSearch/PostsSearch";
 
 export const PostsFeed = () => {
     const {posts, isLoading} = usePostsFeed();
 
     return (
-        <div className={clsx([Styles.posts, 'grid'])}>
-            {isLoading ? <span>{TEXTS.loadingText}</span> : posts.map((post: Post) => <PostFeedItem key={post.id}
-                                                                                                    post={post}/>)}
+        <div className={Styles.postsContainer}>
+            {isLoading ? <span>{TEXTS.loadingText}</span> :
+                <PostsSearch posts={posts}>
+                    {(filteredPosts: Post[]) =>
+                        <div className={clsx([Styles.posts, 'grid'])}>
+                            {filteredPosts.map((post: Post) => <PostFeedItem
+                                key={post.id}
+                                post={post}/>)}
+                        </div>}
+                </PostsSearch>
+            }
         </div>
     )
 }
