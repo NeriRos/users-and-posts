242529960c7dbea85c40606775cd47021f7be1dd
Neriya Rosner <neriyarosner@gmail.com>
2023-07-16 19:09:00 +0300

    design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import Styles from '@/styles/Home.module.css'
+import {useRouter} from "next/router";
 
 
 export default function Home() {
+    const router = useRouter()
+    const getStartedClick = () => {
+        router.push('/users-management')
+    }
+
     return (
         <>
             <h1 className={Styles.projectName}>Users and Posts</h1>
             <div className={Styles.description}>
                 <p>A simple web app that displays users and their posts</p>
             </div>
-            <button className={Styles.button}>Get Started</button>
+            <button className={Styles.getStarted} onClick={getStartedClick}>Get Started</button>
         </>
     )
 }
