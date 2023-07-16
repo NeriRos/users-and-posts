a01956ef841f0e224f7d5314f285161d0b82e0ef
Neriya Rosner <neriyarosner@gmail.com>
2023-07-16 15:36:04 +0300

    create table row

---
+++ src/features/users/modules/UsersManagement/components/UsersTable/UsersTableRow.tsx
@@ -0,0 +1,18 @@
+import {User} from "@/features/users/models/User";
+import {AddressText} from "@/core/components/Address";
+import {useUsersTable} from "@/features/users/modules/UsersManagement/components/UsersTable/useUsersTable";
+
+export const UsersTableRow = ({user}: { user: User }) => {
+    const {onUserSelected} = useUsersTable();
+
+    return (
+        <tr>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td><AddressText address={user.address}/></td>
+            <td className={"actions"}>
+                <button onClick={(e) => onUserSelected(user, e)}>Select</button>
+            </td>
+        </tr>
+    )
+}
