e67da6dd55b327bec5bb4b6d30b40fc0de9348c0
Neriya Rosner <neriyarosner@gmail.com>
2023-07-17 15:01:39 +0300

    design header

--- src/core/components/Layout/Header/Header.tsx
+++ src/core/components/Layout/Header/Header.tsx
@@ -3,12 +3,16 @@ import Styles from './Header.module.css';
 import Link from "next/link";
 import {LINKS} from "@/core/components/Layout/Header/consts";
 import clsx from "clsx";
+import {useRouter} from "next/router";
 
 export const Header = (props: { className: string }) => {
+    const router = useRouter();
+
     return <div className={clsx([Styles.header, props.className])}>
         <nav className={Styles.menu}>
             {LINKS.map((link) =>
-                <Link className={Styles.link} href={link.href} key={link.href}>{link.label}</Link>
+                <Link className={clsx([Styles.link, router.pathname === link.href && Styles.active])} href={link.href}
+                      key={link.href}>{link.label}</Link>
             )}
         </nav>
     </div>;
