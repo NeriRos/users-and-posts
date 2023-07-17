93771d7b16ee6f855653f5929ddeccd2aae40ced
Neriya Rosner <neriyarosner@gmail.com>
2023-07-17 12:36:55 +0300

    create column sort

---
+++ src/features/users/pages/users-management/components/UsersTable/components/ColumnSort.tsx
@@ -0,0 +1,29 @@
+import {SORT_DIRECTION} from "@/features/users/pages/users-management/components/UsersTable/consts";
+
+import Styles from '../UsersTable.module.css';
+import {FaChevronDown, FaChevronUp} from "react-icons/fa";
+
+export const ColumnSort = (props: { sortKey: string, onChange: (direction: string, key: string) => void }) => {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange(e.target.value, props.sortKey)
+    }
+
+    const name = `sort-${props.sortKey}`;
+
+    return <div className={Styles.tableSorting}>
+        <label>
+            <FaChevronUp/>
+            <input type="radio"
+                   name={name}
+                   value={SORT_DIRECTION.ASC}
+                   onChange={onChange}/>
+        </label>
+        <label>
+            <FaChevronDown/>
+            <input type="radio"
+                   name={name}
+                   value={SORT_DIRECTION.DESC}
+                   onChange={onChange}/>
+        </label>
+    </div>
+}
